a720bfa86b381c1a4c1f0be73e8d76b47ca369bf
Upadrasta Chandra Sekhar Pavan Kumar <pavanupadrasta@gmail.com>
2024-10-25 16:30:50 +0000

    modified edq

--- DevBase.py
+++ DevBase.py
@@ -1,21 +1,45 @@
 import requests
 import config
-import s3_handler
+
 
 
 class DevBase:
     def __init__(self) -> None:
-        self.s3_handler = s3_handler()
-
         self.base_url = config.default_config.base_url
         self.gateway  = config.default_config.gateway
-        
+        self.headers={"Content-Type":"application/json"}
         pass
     
+    def _create_dataset(self,**kwargs):
+        create_dataset = config.default_config.dataset_config.create_dataset
+        response = requests.post(f"{self.base_url}{self.gateway}{create_dataset}",params=kwargs,headers=self.headers)
+        if response.status_code == 200:
+            return response
+        else:
+            return {'status':'Invalid Response'}
+        
     def _create_job_configuration(self,**kwargs):
-
         create_job = config.default_config.job_config.create_job
+        response = requests.post(f"{self.base_url}{self.gateway}{create_job}",params=kwargs,headers=self.headers)
+        if response.status_code == 200:
+            return response
+        else:
+            return {'status':'Invalid Response'}
 
-        response = requests.post(f"{self.base_url}{self.gateway}{create_job}",params=kwargs)
+    def _create_rule_set(self,**kwargs):
+        create_rule_set = config.default_config.rule_config.create_ruleset
+        response = requests.post(f"{self.base_url}{self.gateway}{create_rule_set}",params=kwargs,headers=self.headers)
+        if response.status_code == 200:
+            return response
+        else:
+            return {'status':'Invalid Response'}
+    
+    def _create_rule(self,**kwargs):
+        create_rule_set = config.default_config.rule_config.create_ruleset
+        response = requests.post(f"{self.base_url}{self.gateway}{create_rule_set}",params=kwargs,headers=self.headers)
+        if response.status_code == 200:
+            return response
+        else:
+            return {'status':'Invalid Response'}
 
 
--- EDQClientRest.py
+++ EDQClientRest.py
@@ -1,6 +1,23 @@
-class EDQClientRest:
+from DevBase import DevBase
+import config
+class EDQClientRest(DevBase):
     def __init__(self):
-        pass
+        super().__init__(self)
 
-    def _get_job_config(self):
-        pass
+    def _create_job(self,job_name):
+
+        """Creation of Job with request body
+            with datasetId and job Name
+        
+        """
+        params = {'datasetConfigurationId':config.default_config.datasets[config.default_config.datasets.keys()[0]],
+                  'job_name':job_name}
+        json_response = self._create_job_configuration(params)
+        return {"status":f"Job ID created: {json_response['jobId']}"}
+    
+
+    def _create_dataset(self,file_list):
+        params = {'datasetConfigurationId':config.default_config.datasets[config.default_config.datasets.keys()[0]],
+                  'job_name':job_name}
+        json_response = self._create_job_configuration(params)
+        return {"status":f"Job ID created: {json_response['jobId']}"}
