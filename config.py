31680b5498a1e5770568e9ce15a69e9f7e6a0975
Upadrasta Chandra Sekhar Pavan Kumar <pavanupadrasta@gmail.com>
2024-10-25 13:13:19 +0000

    config created

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 default_config = {
     "base_url":'http://www.google.com/',
     'gateway':'/internal-operations.data-management/data-quality-configuration'
-    'job_config":{
+    "job_config":{
 
 
     }
