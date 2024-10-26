829d0d8aa7d0a5ca46210182eb55cd0021628e78
Upadrasta Chandra Sekhar Pavan Kumar <pavanupadrasta@gmail.com>
2024-10-26 17:51:40 +0000

    cleaning up the code with docstring

--- EDQClientRest.py
+++ EDQClientRest.py
@@ -17,7 +17,41 @@ class EDQClientRest(DevBase):
     
 
     def _create_dataset(self,file_list):
-        params = {'datasetConfigurationId':config.default_config.datasets[config.default_config.datasets.keys()[0]],
-                  'job_name':job_name}
+        
+        """Creation of Job with request body
+            with as filelist
+        
+        """
+        params = {'filelist':file_list}
+        json_response = self._create_job_configuration(params)
+        return {"status":f"Job ID created: {json_response['datasetConfigurationId']}"}
+    
+
+    def _create_ruleset(self,file_list):
+        """
+        create ruleset and get the ruleset Id
+        """
+        #YTD input params
+        params = {} 
+        json_response = self._create_job_configuration(params)
+        return {"status":f"Job ID created: {json_response['rulesetId']}"}
+    
+    def _create_rule(self,rules):
+        """
+        creation of rules and attaching rules to ruleset
+        
+        """
+        params = {'rules':rules}
         json_response = self._create_job_configuration(params)
         return {"status":f"Job ID created: {json_response['jobId']}"}
+    
+    def _schedule_job(self,jobId):
+        """
+        Job Schedule based on Job  Id
+        
+        """
+        params = {'datasetConfigurationId':config.default_config.datasets[config.default_config.datasets.keys()[0]],
+                  'jobId':jobId}
+        json_response = self._create_job_configuration(params)
+        return {"status":f"Job ID created: {json_response['jobId']}"}
+    
